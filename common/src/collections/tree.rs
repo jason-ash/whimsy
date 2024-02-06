67aec101b4fe2dc2f99116cd17ee757690571043
Jason Ash <jason.ash@symetra.com>
2024-02-05 16:55:38 -0800

    fleshing out the tree functionality

--- common/src/collections/tree.rs
+++ common/src/collections/tree.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Default)]
 pub struct Tree<T> {
     nodes: Vec<T>,
-    parents: Vec<usize>,
+    parents: Vec<Option<usize>>,
     children: Vec<Vec<usize>>,
 }
 
@@ -14,13 +14,12 @@ impl<T> Tree<T> {
         self.nodes.get_mut(node_id)
     }
 
-    pub fn get_parent_id(&self, node_id: usize) -> Option<&usize> {
-        self.parents.get(node_id)
+    pub fn get_parent_id(&self, node_id: usize) -> Option<usize> {
+        self.parents.get(node_id)?.to_owned()
     }
 
     pub fn get_parent_node(&self, node_id: usize) -> Option<&T> {
-        self.get_parent_id(node_id)
-            .and_then(|&id| self.nodes.get(id))
+        self.parents.get(node_id)?.and_then(|id| self.nodes.get(id))
     }
 
     pub fn get_children_ids(&self, node_id: usize) -> Option<&[usize]> {
@@ -28,7 +27,53 @@ impl<T> Tree<T> {
     }
 
     pub fn get_children_nodes(&self, node_id: usize) -> Option<Vec<&T>> {
-        self.get_children_ids(node_id)
+        self.children
+            .get(node_id)
             .and_then(|ids| ids.iter().map(|&id| self.nodes.get(id)).collect())
+    }
+
+    pub fn add_node(&mut self, node: T, parent_id: Option<usize>) -> Option<usize> {
+        let node_id = self.nodes.len();
+
+        // if the node claims to have a parent, validate that it exists;
+        // if it does, then add the new node to the children array of the parent.
+        if let Some(parent_id) = parent_id {
+            self.children.get_mut(parent_id)?.push(node_id);
+        }
+
+        // next, add the node to the nodes array,
+        // add an empty vector to hold its children,
+        // and add a None value to the parents array.
+        self.nodes.push(node);
+        self.parents.push(parent_id);
+        self.children.push(Vec::new());
+
+        Some(node_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Default)]
+    struct Node(u64);
+
+    #[test]
+    fn get_node_after_insert() {
+        let mut tree = Tree::default();
+        let node_id = tree.add_node(Node(0), None).unwrap();
+        let node_ref = tree.get_node(0);
+        assert!(matches!(Some(&Node(0)), node_ref));
+    }
+
+    fn validate_simple_tree() {
+        let mut tree = Tree::default();
+        let parent_id = tree.add_node(Node(0), None);
+        let child1_id = tree.add_node(Node(1), parent_id);
+        let child2_id = tree.add_node(Node(2), parent_id);
+        let child3_id = tree.add_node(Node(3), child1_id);
+        let child4_id = tree.add_node(Node(4), child2_id);
+        let child5_id = tree.add_node(Node(5), child2_id);
     }
 }
