aada5caac9ae61b8abf0ad30be13ddcf16a6f2ef
Jason Ash <jason.ash@symetra.com>
2024-02-05 20:49:30 -0800

    add more traits for MCTS

--- common/src/algorithms/mcts.rs
+++ common/src/algorithms/mcts.rs
@@ -1,4 +1,6 @@
-pub trait MonteCarloTreeSearchNode {
+/// implement Node on the objects stored in the tree, for example a game state, and
+/// make sure it has fields like `visits` and `score` for the mcts calculations.
+pub trait Node {
     fn score(&self) -> f64;
     fn visits(&self) -> u64;
     fn uct(&self, c: f64, parent_visits: u64) -> f64 {
@@ -10,4 +12,24 @@ pub trait MonteCarloTreeSearchNode {
             w / n + c * ((parent_visits as f64).ln() / n).sqrt()
         }
     }
+}
+
+pub trait Select {
+    fn select(&self) -> usize;
+}
+
+pub trait Expand {
+    fn expand(&mut self, node_id: usize) -> usize;
+}
+
+pub trait Simulate {
+    fn simulate(&self, node_id: usize) -> usize;
+}
+
+pub trait Backpropagate {
+    fn backpropagate(&mut self) -> usize;
+}
+
+pub trait MonteCarloTreeSearch: Select + Expand + Simulate + Backpropagate {
+    fn run(&mut self, iterations: u32);
 }
