02089261dec96598a9c3460e8cb98cad6ef741e0
Jason Ash <jason.ash@symetra.com>
2024-02-05 16:26:02 -0800

    add more methods to the tree

--- common/src/collections/tree.rs
+++ common/src/collections/tree.rs
@@ -10,11 +10,25 @@ impl<T> Tree<T> {
         self.nodes.get(node_id)
     }
 
+    pub fn get_node_mut(&mut self, node_id: usize) -> Option<&mut T> {
+        self.nodes.get_mut(node_id)
+    }
+
     pub fn get_parent_id(&self, node_id: usize) -> Option<&usize> {
         self.parents.get(node_id)
     }
 
-    pub fn get_children(&self, node_id: usize) -> Option<&[usize]> {
+    pub fn get_parent_node(&self, node_id: usize) -> Option<&T> {
+        self.get_parent_id(node_id)
+            .and_then(|&id| self.nodes.get(id))
+    }
+
+    pub fn get_children_ids(&self, node_id: usize) -> Option<&[usize]> {
         self.children.get(node_id).map(Vec::as_slice)
+    }
+
+    pub fn get_children_nodes(&self, node_id: usize) -> Option<Vec<&T>> {
+        self.get_children_ids(node_id)
+            .and_then(|ids| ids.iter().map(|&id| self.nodes.get(id)).collect())
     }
 }
